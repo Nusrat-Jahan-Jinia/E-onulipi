e1c24df5a01d7b7d72589e93b5072b6f83248e8f
nusrat <nusratcode14@gmail.com>
2024-07-09 18:22:06 +0600

    Add controllers and routes of division, district, court and petition

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourtController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DivisionController;
+use App\Http\Controllers\DistrictController;
+use App\Http\Controllers\PetitionController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('courts', CourtController::class);
+
+Route::get(
+    '/user/profile',
+    [UserController::class, 'show']
+)->name('profile');
+
+Route::controller(PetitionController::class)->group(function () {
+    Route::get('/petitions/{id}', 'show');
+    Route::post('/petitions', 'list');
+    Route::get('/petitions', 'list');
+    Route::delete('/petitions/{id}', 'delete');
+    Route::patch('/petitions/{id}', 'patch');
+});
+
+Route::controller(DivisionController::class)->group(function () {
+    Route::get('/divisions', 'list');
+});
+
+Route::controller(DistrictController::class)->group(function () {
+    Route::get('/districts', 'list');
 });
